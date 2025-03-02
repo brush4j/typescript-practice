3786943c23c773e7e004eaa36b4219256d8431fe
Halfmoonly <liuyanoutsee@outlook.com>
2025-03-02 15:49:08 +0800

    typescript调试，vscode环境配置

---
+++ index.ts
@@ -0,0 +1,17 @@
+
+import { fn1 } from './src/mock';
+fn1();
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+const myMapTs = new Map<string, number>([
+    ['a', 1],
+    ['b', 2],
+    ['c', 3]
+]);
+
+for (const [key, value] of myMapTs) {
+    console.log(`Key: ${key}, Value: ${value}`);
+}
+
---
+++ src/mock.ts
@@ -0,0 +1,9 @@
+function fn1() {
+console.log(`我是mock.ts文件#fn1()`);
+}
+
+function fn2() {
+    console.log(`我是mock.ts文件#fn2()`);
+    }
+
+export {fn1,fn2};
